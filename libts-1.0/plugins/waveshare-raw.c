ebcae78e0fdee99751b4bab99949001d3292feac
Ralph Irving <ralph.irving@gmail.com>
2017-04-20 15:22:08 -0400

    Better support variants that return less/more touch info within the limits of the bit field and allow the ts device to be closed and reopened. Propogate approprate errors so manging application can reopen or abort as nessary.

--- libts-1.0/plugins/waveshare-raw.c
+++ libts-1.0/plugins/waveshare-raw.c
@@ -1,10 +1,40 @@
 /*
  * tslib driver for WaveShare touchscreens
  * Copyright (C) 2015 Peter Vicman
+ * Copyright (C) 2015 Phillip Camp
  * inspiration from derekhe: https://github.com/derekhe/wavesahre-7inch-touchscreen-driver
  *
  * This file is placed under the LGPL.  Please see the file COPYING for more
  * details.
+ *
+ * usb 6-2: New USB device found, idVendor=0eef, idProduct=0005
+ * usb 6-2: New USB device strings: Mfr=1, Product=2, SerialNumber=3
+ * usb 6-2: Product: By ZH851
+ * usb 6-2: Manufacturer: RPI_TOUCH
+ *
+ * Touch events consists of ~25, one example is
+ * aa 01 03 1b 01 d2 bb 03 01 68 02 cc 00 5d 01 ef 01 5f 01 fe 00 fb 02 37 cc
+ * Offset:
+ *     0 : Start byte (aa)
+ *     1 : Any touch (0=off,1=on)
+ *   2-3 : First touch X
+ *   4-5 : First touch Y
+ *     6 : Multi-touch start (bb)
+ *     7 : Bitmask for all touches (bit 0-4 (first-fifth), 0=off, 1=on)
+ *   8-9 : Second touch X
+ * 10-11 : Second touch Y
+ * 12-13 : Third touch X
+ * 14-15 : Third touch Y
+ * 16-17 : Fourth touch X
+ * 18-19 : Fourth touch Y
+ * 20-21 : Fifth touch X
+ * 22-23 : Fifth touch Y
+ *    24 : End byte (cc or 00)
+ *
+ * Seen screens with 19 and 22 bytes...
+ * Driver default allows up to 7 point touch (29bytes) - bit field limit in protocol
+ * We only ever process the first touch atm.
+ *
  */
 
 #include <stdlib.h>
@@ -22,6 +52,8 @@
 #include "config.h"
 #include "tslib-private.h"
 
+#define DEBUG
+
 struct tslib_input {
   struct tslib_module_info module;
   int len;
@@ -32,42 +64,67 @@ static int waveshare_read(struct tslib_module_info *inf, struct ts_sample *samp,
   struct tslib_input *i = (struct tslib_input *) inf;
   struct tsdev *ts = inf->dev;
   char *buf;
-  int ret;
+  int ret, rs, count = 0;
 
   buf = alloca(i->len * nr);
-
   ret = read(ts->fd, buf, i->len * nr);
-  if(ret > 0) {
-    while(ret >= (int) i->len) {
+  rs = ret / nr;
+
+  if(ret >= 0) {
+    while( ret >= 13 && buf[0] == 0xaa && buf[6] == 0xbb ) {
       /*
         0000271: aa01 00e4 0139 bb01 01e0 0320 01e0 0320 01e0 0320 01e0 0320 cc  .....9..... ... ... ... .
 
         "aa" is start of the command, "01" means clicked while "00" means unclicked.
         "00e4" and "0139" is the X,Y position (HEX).
         "bb" is start of multi-touch, and the following bytes are the position of each point.
+	FIXME check for end byte but we read multiple records here...
+	((rs == 22 || rs == 25  && (buf[rs-1] == 00 || buf[rs-1] == 0xcc))
       */
       samp->pressure = buf[1] & 0xff;
       samp->x = ((buf[2] & 0xff) << 8) | (buf[3] & 0xff);
       samp->y = ((buf[4] & 0xff) << 8) | (buf[5] & 0xff);
       gettimeofday(&samp->tv, NULL);
+
 #ifdef DEBUG
-        fprintf(stderr, "waveshare raw: %d %d %d\n", samp->x, samp->y, samp->pressure);
-        fprintf(stderr, "%x %x %x %x %x %x\n", buf[0], buf[1], buf[2], buf[3], buf[4], buf[5]);
+      fprintf(stderr, "waveshare: size %x/%x %d\n", ret, rs, nr);
+      fprintf(stderr, "waveshare: raw %x %x %x %x %x\n", buf[1], buf[2], buf[3], buf[4], buf[5]);
+      fprintf(stderr, "waveshare: sample %dx%d p %d\n", samp->x, samp->y, samp->pressure);
 #endif
       samp++;
-      buf += i->len;
-      ret -= i->len;
+      count++;
+      buf += rs;
+      ret -= rs;
     }
   } else {
+    if (errno == EAGAIN) {
+	//break;
+	return count;
+    } else if (errno == EINTR) {
+	//continue;
+	return count;
+    }
+#ifdef DEBUG
+    fprintf(stderr, "waveshare: error %d %d\n", ret, errno);
+#endif
     return -1;
   }
+#ifdef DEBUG
+  fprintf(stderr, "waveshare: count %d/%d\n", count , nr);
+#endif
+  return count;
+}
 
-  return nr;
+static int waveshare_fini(struct tslib_module_info *inf)
+{
+	free(inf);
+	return 0;
 }
 
 static const struct tslib_ops waveshare_ops =
 {
   .read = waveshare_read,
+  .fini = waveshare_fini,
 };
 
 static int parse_len(struct tslib_module_info *inf, char *str, void *data)
@@ -111,7 +168,7 @@ TSAPI struct tslib_module_info *waveshare_mod_init(struct tsdev *dev, const char
     return NULL;
 
   i->module.ops = &waveshare_ops;
-  i->len = 25;
+  i->len = 29;
 
   if (tslib_parse_vars(&i->module, raw_vars, NR_VARS, params)) {
     free(i);
